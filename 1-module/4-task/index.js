c8e5bd576720c9feddf27d0af5358b2c0d9122ea
Alexey Suprun <pers-fes@yandex.ru>
2022-02-16 20:37:33 +0300

    Решил 5 задач Занятия 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  if (str.indexOf('1xBet'.toLowerCase()) != -1 || str.indexOf('XXX'.toLowerCase()) != -1) {
+     return true;
+  } else {
+      return false;
+  }
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
-function truncate(str, maxlength) {
-  // ваш код...
+function truncate(str, maxlenght) {
+  if (str.length <= 20) {
+      return `${str}`;
+  } else { 
+      return `${str.slice(0, maxlenght - 1) + '…'}`;
+
+  }
 }
