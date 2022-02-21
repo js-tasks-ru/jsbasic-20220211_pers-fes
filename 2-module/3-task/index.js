0c2ce38aec31715fb831b83880c90c04adb27cd5
Alexey Suprun <pers-fes@yandex.ru>
2022-02-21 21:50:43 +0300

    Решил 3 задачи Занятия 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
-let calculator = {
-  // ваш код
+const calculator = {
+  read(a, b) {
+      this.a = a;
+      this.b = b;
+  },
+  sum() {
+      let result = this.a + this.b;
+      return result;
+  },
+  mul() {
+      let result = this.a * this.b;
+      return result;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
