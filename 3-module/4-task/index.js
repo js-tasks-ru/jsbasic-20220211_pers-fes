a994b9d4bfaeb6291cb0cfc8ddc35af7083b14bb
Alexey Suprun <pers-fes@yandex.ru>
2022-03-09 21:31:33 +0300

    Решил 5 задач Занятия 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,5 @@
 function showSalary(users, age) {
-  // ваш код...
+  let arr = users.filter(user => user.age <= age).map(user => `${user["name"]}, ${user["balance"]}`);
+  return arr.join('\n');
+  
 }
